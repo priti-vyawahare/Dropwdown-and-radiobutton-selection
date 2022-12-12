1266e3fe8c2c485ef2f63dfa62c018a40e620f6f
priti vyawahare <pritiv.acem@gmail.com>
2022-12-12 11:27:13 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+
+namespace open_tab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://aquayara.nl/zwemscore-inschrijven/");
+          IWebElement elementgender = driver.FindElement(By.Id("geslachtm"));
+            elementgender.Click();
+
+            IWebElement dropdwon = driver.FindElement(By.Id("locatie"));
+            SelectElement dropdownlist = new SelectElement(dropdwon);
+            dropdownlist.SelectByText("Zwemschool Aquayara Tussen de Vaarten B.V.");
+
+            IWebElement elementbetaal = driver.FindElement(By.Id("betaalmethodeo"));
+            elementbetaal.Click();
+          
+        }
+    }
+}
